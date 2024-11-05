aa6b05c4c29120a6fa7788c5f14292364d94a5ab
Caden McVey <59580833+caden602@users.noreply.github.com>
2024-11-05 14:29:41 -0700

    Add basic dialog system

---
+++ scripts/dialog_area.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var text_key: String = ""
+
+var area_active: bool = false
+
+func _input(event):
+	if area_active and event.is_action_pressed("space"):
+		print("space")
+		SignalBus.emit_signal("display_dialog", text_key)
+		
+func _on_DialogArea_area_entered(_area):
+	area_active = true
+	
+func _on_DialogArea_area_exited(_area):
+	area_active = false
