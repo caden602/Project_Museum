b938ad6605603a386e03c01b46e977b3c950cd34
Caden McVey <59580833+caden602@users.noreply.github.com>
2024-12-15 12:41:03 -0700

    add second sister dialog

---
+++ scripts/secondsister.gd
@@ -0,0 +1,62 @@
+extends CharacterBody2D
+
+# Store the initial position to maintain the bobbing centered
+var initial_position: Vector2
+
+var is_chatting = false
+var player_in_area = false
+
+# Function to start the bobbing effect
+func start_bobbing():
+	var tween: Tween = get_tree().create_tween()
+	
+	# Define target position to move up by 1 pixel from the initial position
+	var target_position = initial_position + Vector2(0, -1)  # Move up by 1 pixel
+
+	# Animate up
+	tween.tween_property($Sprite2D, "position", target_position, 0.5)
+	
+	# Connect the "finished" signal to alternate bobbing direction
+	tween.finished.connect(_bob_down)
+
+# Callback to animate the sprite down
+func _bob_down():
+	var tween: Tween = get_tree().create_tween()
+	
+	# Move down by 1 pixel from the initial position
+	var target_position = initial_position + Vector2(0, 1)  # Move down by 1 pixel
+	tween.tween_property($Sprite2D, "position", target_position, 0.5)
+	
+	# Connect back to start the upward motion
+	tween.finished.connect(start_bobbing)
+
+func _ready():
+	# Set the initial position to the sprite's starting position
+	initial_position = $Sprite2D.position
+	#start_bobbing()
+	Dialogic.signal_event.connect(DialogicSignal)
+	
+func _process(delta):
+	if player_in_area:
+		$AnimationPlayer.play("phar_front")
+		if Input.is_action_just_pressed("e"):
+			run_dialog("secondsister_timeline")
+	else:
+		$AnimationPlayer.play("phar_back")
+func run_dialog(dialogue_string):
+	is_chatting = true
+	Dialogic.start(dialogue_string)	
+	
+func DialogicSignal(arg: String):
+	if arg == "end_game":
+		get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
+	pass
+
+func _on_chat_detection_zone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_area = true
+
+
+func _on_chat_detection_zone_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_area = false
